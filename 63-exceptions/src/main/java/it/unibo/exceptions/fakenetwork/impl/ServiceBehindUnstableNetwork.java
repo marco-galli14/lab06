58c8cd3def237d3c060e9441047aac1aafe36725
Marco Galli <marco.galli14@studio.unibo.it>
2025-10-27 13:03:15 +0100

    Risolo primo punto esercizio 63-EXEPTIONS, DA FINIRE: punti rimanenti es 63, TUTTO es 64, TUTTO es 65

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,6 +29,9 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
+        if((failProbability < 0) || (failProbability >= 1)) {
+            throw new IllegalArgumentException("failProbability must be in [0, 1[ !");
+        }
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
