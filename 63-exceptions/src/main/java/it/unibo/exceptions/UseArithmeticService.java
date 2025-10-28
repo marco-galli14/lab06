226016f3e85ff54aba9720ccee8558ba25ffadad
Marco <marco.galli14@studio.unibo.it>
2025-10-28 16:00:31 +0100

    risolto punto 2 esercizio 63

--- 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
@@ -3,6 +3,7 @@ package it.unibo.exceptions;
 import it.unibo.exceptions.fakenetwork.api.NetworkComponent;
 import it.unibo.exceptions.fakenetwork.impl.ServiceBehindUnstableNetwork;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static it.unibo.exceptions.arithmetic.ArithmeticService.DIVIDED;
@@ -48,6 +49,11 @@ public final class UseArithmeticService {
          * This method should re-try to send message to the provided server, catching all IOExceptions,
          * until it succeeds.
          */
+        try {
+           server.sendData(message); 
+        } catch (IOException e) {
+            retrySendOnNetworkError(server,message);
+        }
     }
 
     private static String retryReceiveOnNetworkError(final NetworkComponent server) {
@@ -55,7 +61,11 @@ public final class UseArithmeticService {
          * This method should re-try to retrieve information from the provided server, catching all IOExceptions,
          * until it succeeds.
          */
-        return null;
+        try {
+            return server.receiveResponse();
+        } catch (IOException e) {
+            return retryReceiveOnNetworkError(server);
+        }
     }
 
     private static void assertEqualsAsDouble(final String expected, final String actual) {
